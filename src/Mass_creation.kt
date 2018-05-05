d6113f8d27079d5b2c0730dd74270c0d3fea692d
jellyfish26 <jellyfish026@outlook.jp>
2018-05-06 03:50:50 +0900

    [Change] Change initial transition

---
+++ src/Mass_creation.kt
@@ -0,0 +1,35 @@
+import javafx.stage.Stage
+import java.awt.Button
+import java.util.*
+
+object Mass_creation {
+
+    //2-dimensional array
+    var temp = Array(12, {Array(12,{0})})
+    var point: Array<Array<Int>> = temp
+
+    //create square
+    fun create(vertical: Int, width: Int, this_stage: Stage?) {
+
+    }
+
+    // odd odd
+    fun odd_odd_random() {
+
+    }
+
+    //odd even
+    fun odd_even_randon() {
+
+    }
+
+    //even odd
+    fun even_odd_random() {
+
+    }
+
+    //even even
+    fun even_even_random() {
+
+    }
+}
