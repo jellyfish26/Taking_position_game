5d81d5e53e141475a84f37ea032145b4c4ed3601
jellyfish26 <jellyfish0162@gmail.com>
2018-05-06 03:50:50 +0900

    [Change] Change initial transition

--- src/App.kt
+++ src/App.kt
@@ -1,9 +1,11 @@
 import javafx.application.Application
+import javafx.application.Platform
 import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
 import javafx.scene.Scene
 import javafx.scene.control.*
 import javafx.stage.Stage
+import java.util.*
 import kotlin.system.exitProcess
 
 //gui class
@@ -11,62 +13,67 @@ class App : Application() {
 
     //start GUI
     override fun start(primaryStage: Stage?) {
-        //title
         primaryStage?.title = "Taking position game"
 
         //scene (deleted)
         //primaryStage?.scene = Scene(FXMLLoader.load<Parent>(this.javaClass.getResource("App.fxml")))
 
-        //show gui
         primaryStage?.show()
 
         Log.main("launch")
-        initialization()
+        var vertical = initialization(primaryStage,"Please enter the number of vertical squares. (5 to 12)")
+        var width = initialization(primaryStage, "Please enter the number of width squares. (5 to 12)")
+
+        Mass_creation.create(vertical, width, primaryStage)
     }
 
     //Display a dialog for generating a square.
-    private fun initialization() {
+    private fun initialization(this_stage: Stage?, content: String?): Int {
         // Input dialog display
-        var result_square = Dialog.InputDialog(null, "Create square", "Please enter the size of the square to be generated.(5 to 12)")
+        var resultsquare = Dialog.InputDialog(null, "Create square", content)
 
-        if (result_square == null) exitProcess(0) // End of program
+        var number = 0
 
-        // judge alert
-        if (result_square == "") {
+        if (resultsquare == null) exitProcess(0) // End of program, No further processing is performed.
+
+        // judge alerthorizontal
+        if (resultsquare == "") {
             //warning dialog display
             var warning = Dialog.AlertDialog(Alert.AlertType.WARNING, null, "Warning", "Please enter a number (5 to 12)")
 
             if (warning == ButtonType.OK) {
                 // return
-                initialization()
+                initialization(this_stage, content)
             } else {
-                exitProcess(0)
+                Platform.exit()
             }
         } else {
-            var number: Int? = 0
-            try {
-                number = result_square?.toInt()
-            } catch (e: NumberFormatException) {
-                var number_warning = Dialog.AlertDialog(Alert.AlertType.WARNING, null, "Warning", "Please enter a number (5 to 12)")
 
-                if (number_warning == ButtonType.OK) {
+            try {
+                number = resultsquare.toInt()
+            } catch (e: NumberFormatException) {
+                var numberwarning = Dialog.AlertDialog(Alert.AlertType.WARNING, null, "Warning", "Please enter a number (5 to 12)")
+
+                if (numberwarning == ButtonType.OK) {
                     // return
-                    initialization()
+                    initialization(this_stage, content)
                 } else {
-                    exitProcess(0)
+                    Platform.exit()
                 }
             }
 
-            if (!(number!! >= 5 && number!! <= 12)) {
-                var out_range = Dialog.AlertDialog(Alert.AlertType.WARNING, null, "Warning", "Please enter a number between 5 and 13")
+            if (!(number >= 5 && number <= 12)) {
+                var outrange = Dialog.AlertDialog(Alert.AlertType.WARNING, null, "Warning", "Please enter a number between 5 and 13")
 
-                if (out_range == ButtonType.OK) {
+                if (outrange == ButtonType.OK) {
                     // return
-                    initialization()
+                    initialization(this_stage, content)
                 } else {
-                    exitProcess(0)
+                    Platform.exit()
                 }
             }
         }
+
+        return number
     }
 }
