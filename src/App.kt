9e45a11411206654c0e3c09f8b032e548c292654
jellyfish26 <jellyfish026@outlook.jp>
2018-05-07 02:01:24 +0900

    [In the process of being created]

--- src/App.kt
+++ src/App.kt
@@ -18,7 +18,7 @@ class App : Application() {
         //scene (deleted)
         //primaryStage?.scene = Scene(FXMLLoader.load<Parent>(this.javaClass.getResource("App.fxml")))
 
-        primaryStage?.show()
+        //primaryStage?.show()
 
         Log.main("launch")
         var vertical = initialization(primaryStage,"Please enter the number of vertical squares. (5 to 12)")
@@ -29,12 +29,37 @@ class App : Application() {
 
     //Display a dialog for generating a square.
     private fun initialization(this_stage: Stage?, content: String?): Int {
-        // Input dialog display
-        var resultsquare = Dialog.InputDialog(null, "Create square", content)
 
         var number = 0
 
+        // Input dialog display
+        var resultsquare = Dialog.InputDialog(null, "Create square", content)
+
         if (resultsquare == null) exitProcess(0) // End of program, No further processing is performed.
+
+        try {
+            number = resultsquare.toInt()
+        } catch (e: NumberFormatException) {
+            var numberwarning = Dialog.AlertDialog(Alert.AlertType.WARNING, null, "Warning", "Please enter a number (5 to 12)")
+
+            if (numberwarning == ButtonType.OK) {
+                // return
+                start(this_stage)
+            } else {
+                Platform.exit()
+            }
+        } finally {
+            if (!(number >= 5 && number <= 12)) {
+                var outrange = Dialog.AlertDialog(Alert.AlertType.WARNING, null, "Warning", "Please enter a number between 5 and 13")
+
+                if (outrange == ButtonType.OK) {
+                    // return
+                    start(this_stage)
+                } else {
+                    Platform.exit()
+                }
+            }
+        }
 
         // judge alerthorizontal
         if (resultsquare == "") {
@@ -43,37 +68,14 @@ class App : Application() {
 
             if (warning == ButtonType.OK) {
                 // return
-                initialization(this_stage, content)
+                start(this_stage)
             } else {
                 Platform.exit()
             }
         } else {
-
-            try {
-                number = resultsquare.toInt()
-            } catch (e: NumberFormatException) {
-                var numberwarning = Dialog.AlertDialog(Alert.AlertType.WARNING, null, "Warning", "Please enter a number (5 to 12)")
-
-                if (numberwarning == ButtonType.OK) {
-                    // return
-                    initialization(this_stage, content)
-                } else {
-                    Platform.exit()
-                }
-            }
-
-            if (!(number >= 5 && number <= 12)) {
-                var outrange = Dialog.AlertDialog(Alert.AlertType.WARNING, null, "Warning", "Please enter a number between 5 and 13")
-
-                if (outrange == ButtonType.OK) {
-                    // return
-                    initialization(this_stage, content)
-                } else {
-                    Platform.exit()
-                }
-            }
         }
 
+        if(Debug.developer) println(number)
         return number
     }
 }
--- src/Mass_creation.kt
+++ src/Mass_creation.kt
@@ -1,4 +1,8 @@
+import javafx.scene.Group
+import javafx.scene.Scene
+import javafx.scene.layout.GridPane
 import javafx.stage.Stage
+import org.w3c.dom.Text
 import java.awt.Button
 import java.util.*
 
@@ -7,29 +11,50 @@ object Mass_creation {
     //2-dimensional array
     var temp = Array(12, {Array(12,{0})})
     var point: Array<Array<Int>> = temp
+    var temp2 = Array(12, { arrayOfNulls<Button>(12)})
+    var btn: Array<Array<Button?>> = temp2
+    var panel: GridPane = GridPane()
 
     //create square
     fun create(vertical: Int, width: Int, this_stage: Stage?) {
+        random(vertical, width)
 
-    }
+        for(ver_length in 0..vertical) {
+            for(wid_length in 0..width) {
+                btn[ver_length][wid_length] = Button(point[ver_length][wid_length].toString())
+                //panel.add(Button(point[ver_length][wid_length].toString()), ver_length, wid_length)
+            }
+        }
 
-    // odd odd
-    fun odd_odd_random() {
+        panel.layoutY = 50.0
+        val sc = Scene(panel,750.0,750.0)
 
+        this_stage?.scene = sc
+        this_stage?.show()
     }
 
     //odd even
-    fun odd_even_randon() {
+    fun random(vertical: Int, width: Int) {
+        for(ver_length in 0..((vertical / 2))) {
+            for(wid_length in 0..((width / 2))) {
+                point[ver_length][wid_length] = Random().nextInt(32) - 16
 
-    }
+                // Line symmetry
+                point[vertical - ver_length - 1][width - wid_length - 1] = point[ver_length][wid_length]
+                point[vertical - ver_length - 1][wid_length] = point[ver_length][wid_length]
+                point[ver_length][width - wid_length - 1] = point[ver_length][wid_length]
+            }
+        }
 
-    //even odd
-    fun even_odd_random() {
-
-    }
-
-    //even even
-    fun even_even_random() {
-
+        // debug
+        if(Debug.developer) {
+            for(ver_length in 0..(vertical - 1)) {
+                for(wid_length in 0..(width - 1)) {
+                    print(point[ver_length][wid_length].toString() + " ")
+                }
+                println()
+            }
+        }
     }
 }
+
