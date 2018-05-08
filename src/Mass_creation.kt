0459ea074f38575191b4a58f0722c7358f14682b
jellyfish26 <jellyfish026@outlook.jp>
2018-05-09 01:15:56 +0900

    [Progress] Preservation of today's data

--- src/Mass_creation.kt
+++ src/Mass_creation.kt
@@ -1,9 +1,11 @@
 import javafx.scene.Group
+import javafx.scene.Node
 import javafx.scene.Scene
 import javafx.scene.layout.GridPane
 import javafx.stage.Stage
 import org.w3c.dom.Text
 import java.awt.Button
+import java.awt.Label
 import java.util.*
 
 object Mass_creation {
@@ -13,7 +15,7 @@ object Mass_creation {
     var point: Array<Array<Int>> = temp
     var temp2 = Array(12, { arrayOfNulls<Button>(12)})
     var btn: Array<Array<Button?>> = temp2
-    var panel: GridPane = GridPane()
+    var panel = GridPane()
 
     //create square
     fun create(vertical: Int, width: Int, this_stage: Stage?) {
@@ -22,11 +24,10 @@ object Mass_creation {
         for(ver_length in 0..vertical) {
             for(wid_length in 0..width) {
                 btn[ver_length][wid_length] = Button(point[ver_length][wid_length].toString())
-                //panel.add(Button(point[ver_length][wid_length].toString()), ver_length, wid_length)
+                //panel.add(btn[ver_length][wid_length], ver_length, wid_length)
             }
         }
 
-        panel.layoutY = 50.0
         val sc = Scene(panel,750.0,750.0)
 
         this_stage?.scene = sc
