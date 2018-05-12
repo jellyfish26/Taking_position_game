54e579cdf92d780b3ee6558307026e1052b201c3
jellyfish26 <jellyfish0162@gmail.com>
2018-05-12 16:36:49 +0900

    [Stage completed]

--- src/Mass_creation.kt
+++ src/Mass_creation.kt
@@ -1,37 +1,9 @@
-import javafx.scene.Group
-import javafx.scene.Node
-import javafx.scene.Scene
-import javafx.scene.layout.GridPane
-import javafx.stage.Stage
-import org.w3c.dom.Text
-import java.awt.Button
-import java.awt.Label
+import Value.point
 import java.util.*
 
 object Mass_creation {
-
-    //2-dimensional array
-    var temp = Array(12, {Array(12,{0})})
-    var point: Array<Array<Int>> = temp
-    var temp2 = Array(12, { arrayOfNulls<Button>(12)})
-    var btn: Array<Array<Button?>> = temp2
-    var panel = GridPane()
-
-    //create square
-    fun create(vertical: Int, width: Int, this_stage: Stage?) {
+    fun create(vertical: Int, width: Int) {
         random(vertical, width)
-
-        for(ver_length in 0..vertical) {
-            for(wid_length in 0..width) {
-                btn[ver_length][wid_length] = Button(point[ver_length][wid_length].toString())
-                //panel.add(btn[ver_length][wid_length], ver_length, wid_length)
-            }
-        }
-
-        val sc = Scene(panel,750.0,750.0)
-
-        this_stage?.scene = sc
-        this_stage?.show()
     }
 
     //odd even
@@ -58,4 +30,3 @@ object Mass_creation {
         }
     }
 }
-
