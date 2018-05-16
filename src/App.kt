3c870d71c18ebaec9621baa6b9828e5d121e3b12
jellyfish26 <jellyfish026@outlook.jp>
2018-05-17 01:51:51 +0900

    [Change] Random number adjustment

--- src/App.kt
+++ src/App.kt
@@ -30,7 +30,7 @@ class App : Application() {
         val pane = Pane()
         pane.setPrefSize(750.0, 750.0)
 
-        setting()
+        Set.setting()
         val tiles = ArrayList<Tile>()
         for(ver_length in 0 until Value.vertical) {
             for(wid_length in 0 until Value.width) {
@@ -48,55 +48,6 @@ class App : Application() {
         }
 
         return pane
-    }
-
-    private fun setting() {
-
-        if(Debug.developer) println("Lunch App")
-        Value.vertical = initialization("Please enter the number of vertical squares. (5 to 12)")
-        //check
-        while (true) {
-            if(Value.vertical == 0) {
-                Value.vertical = initialization("Please enter the number of vertical squares. (5 to 12)")
-            } else break
-        }
-        Value.width = initialization("Please enter the number of width squares. (5 to 12)")
-
-        //check
-        while (true) {
-            if(Value.vertical == 0) {
-                Value.vertical = initialization("Please enter the number of vertical squares. (5 to 12)")
-            } else break
-        }
-
-        Mass_creation.create(Value.vertical, Value.width)
-    }
-
-    //Display a dialog for generating a square.
-    private fun initialization(content: String?): Int {
-
-        var number = -1
-
-        // Input dialog display
-        var resultsquare = Dialog.InputDialog(null, "Create square", content)
-
-        if (resultsquare == null) exitProcess(0) // End of program, No further processing is performed.
-
-        try {
-            number = resultsquare.toInt()
-
-            if (!(number >= 5 && number <= 12)) {
-                Dialog.AlertDialog(Alert.AlertType.WARNING, null, "Warning", "Please enter a number between 5 and 13")
-                number = 0
-            }
-
-        } catch (e: NumberFormatException) {
-            Dialog.AlertDialog(Alert.AlertType.WARNING, null, "Warning", "Please enter a number (5 to 12)")
-            number = 0
-        }
-
-        if(Debug.developer) println(number)
-        return number //number == 0, false
     }
 
     private inner class Tile(value: String) : StackPane() {
